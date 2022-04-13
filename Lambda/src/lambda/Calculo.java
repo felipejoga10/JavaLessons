c3014628f142413671b9774b26a664a6f9a9f982
Felipe Zanatta <felipe.zanatta@getnet.com.br>
2022-04-13 17:44:04 -0300

    Java Lessons 13 april 2022.

--- Lambda/src/lambda/Calculo.java
+++ Lambda/src/lambda/Calculo.java
@@ -1,6 +1,15 @@
 package lambda;
 
+@FunctionalInterface
 public interface Calculo {
 
     double executar(double a, double b);
+
+    default String legal() {
+        return "nova string";
+    }
+
+    static String muyLegale() {
+        return "muyyLegale";
+    }
 }
